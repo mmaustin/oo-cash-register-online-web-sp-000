5de0dc25822039eecea6267a06de4f5cac29aec9
mccray austin <tubach75@hotmail.com>
2020-10-31 18:48:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items
+
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(item, price, option = 3)
+    @total += price
+  end
+
+end
