4589353fc321af7a3bbba18c30e6443ae198f22b
mccray austin <tubach75@hotmail.com>
2020-10-31 20:01:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,10 +13,16 @@ class CashRegister
   end
 
   def add_item(item, price, option = 3)
-    if option != 0
-      @total += price * option
-    elsif option == 0
-      @total += price
+    if option > 1
+      opts = 1
+      while opts <= option
+        @items << item
+        @total += price
+        opts += 1
+      end
+    else
+      @items << item
+      @tota += price
     end
   end
 
