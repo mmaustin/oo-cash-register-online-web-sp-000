d280513916b710112c7137b7c29ca5a58a688bc1
mccray austin <tubach75@hotmail.com>
2020-10-31 20:11:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,9 @@ class CashRegister
 
   attr_accessor :total, :discount, :items, :last_trans
 
-  def initialize(total = 0)
-    @total = total
-    @discount = 20
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
     @items = []
   end
 
